67f93db0245007303d092d7245de3efc918c5332
Maria Paz <pazguada@gmail.com>
2020-05-29 01:35:56 -0600

    Updates

---
+++ precache-manifest.ee897a353f889b5b6942e0027b38003c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eaadb64ecd5bca9cbfc7360c997435",
+    "url": "/TransformationsGame/index.html"
+  },
+  {
+    "revision": "7db56e49b714c9ce2004",
+    "url": "/TransformationsGame/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b26e21a290a0fb02f433",
+    "url": "/TransformationsGame/static/css/main.75a476e6.chunk.css"
+  },
+  {
+    "revision": "7db56e49b714c9ce2004",
+    "url": "/TransformationsGame/static/js/2.755a1587.chunk.js"
+  },
+  {
+    "revision": "a25424921e199d842bef19e30111a205",
+    "url": "/TransformationsGame/static/js/2.755a1587.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b26e21a290a0fb02f433",
+    "url": "/TransformationsGame/static/js/main.179f3970.chunk.js"
+  },
+  {
+    "revision": "52913a4e916eda9bcb6e",
+    "url": "/TransformationsGame/static/js/runtime-main.1bbefe2a.js"
+  },
+  {
+    "revision": "406210195aaf53ea5f6fd8a4b965bda2",
+    "url": "/TransformationsGame/static/media/Logo1.40621019.png"
+  },
+  {
+    "revision": "7c225a101484200a1a1415c11f7babb4",
+    "url": "/TransformationsGame/static/media/Logo2.7c225a10.png"
+  },
+  {
+    "revision": "d5d100f7f29ab28b8aeec5fedc21be0a",
+    "url": "/TransformationsGame/static/media/Logo3.d5d100f7.png"
+  },
+  {
+    "revision": "7619a833eccef4c3256f318db2a12881",
+    "url": "/TransformationsGame/static/media/Logo4.7619a833.png"
+  },
+  {
+    "revision": "5306784a17a63c368c6e4771cc139f70",
+    "url": "/TransformationsGame/static/media/Logo5.5306784a.png"
+  },
+  {
+    "revision": "4ac8d4f93418c0bcb0a5b0d29a881d9d",
+    "url": "/TransformationsGame/static/media/bounce2.4ac8d4f9.mp3"
+  },
+  {
+    "revision": "155e888fa2af77adb6bcd2cd9c6d9940",
+    "url": "/TransformationsGame/static/media/drop.155e888f.mp3"
+  },
+  {
+    "revision": "ec08b9955c60b303f72831ebf3d19555",
+    "url": "/TransformationsGame/static/media/fire1.ec08b995.mp3"
+  },
+  {
+    "revision": "ffd6b1b4797cebff5447951763709e15",
+    "url": "/TransformationsGame/static/media/fire2.ffd6b1b4.mp3"
+  },
+  {
+    "revision": "9993daaca16bff84412f06fa90962587",
+    "url": "/TransformationsGame/static/media/fire3.9993daac.mp3"
+  },
+  {
+    "revision": "258fd7c217f30a389bf4fc489a9798a8",
+    "url": "/TransformationsGame/static/media/memphis_blue3-01.258fd7c2.png"
+  },
+  {
+    "revision": "5880efea904cb5fabedaeaef54379796",
+    "url": "/TransformationsGame/static/media/move3.5880efea.mp3"
+  },
+  {
+    "revision": "b747cf1f3a6d23668e97004e36f8646a",
+    "url": "/TransformationsGame/static/media/reflectX.b747cf1f.png"
+  },
+  {
+    "revision": "c80b7350335250680f27a386d66137f6",
+    "url": "/TransformationsGame/static/media/reflectionX.c80b7350.png"
+  },
+  {
+    "revision": "a8d0c24f52f423c8f3a1a972ad1542a5",
+    "url": "/TransformationsGame/static/media/reflectionY.a8d0c24f.png"
+  },
+  {
+    "revision": "d19c4174af194e537f202bb8d8328365",
+    "url": "/TransformationsGame/static/media/rotateMin.d19c4174.png"
+  },
+  {
+    "revision": "e7017183218083b881ec0c44b6dbda90",
+    "url": "/TransformationsGame/static/media/rotatePlus.e7017183.png"
+  },
+  {
+    "revision": "7d625e19160c01b9b62b66d6d6d15d57",
+    "url": "/TransformationsGame/static/media/rotation90Clockwise.7d625e19.png"
+  },
+  {
+    "revision": "9c5146a043b6179e2bdc6e550d72bf7f",
+    "url": "/TransformationsGame/static/media/rotation90CounterClockwise.9c5146a0.png"
+  },
+  {
+    "revision": "6e1786e380ee726f73c126253718a33a",
+    "url": "/TransformationsGame/static/media/translate.6e1786e3.png"
+  },
+  {
+    "revision": "3063bcb8850ddc155d1a7b18f5b04aed",
+    "url": "/TransformationsGame/static/media/translationX.3063bcb8.png"
+  }
+]);
